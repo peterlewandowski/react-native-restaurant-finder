49190332dc2135c14d45ce4c8e6e1b0692c3f4be
Peter Lewandowski <mapiotr2010@gmail.com>
2022-02-23 14:34:07 -0500

    adding styling and organizing components

--- App.js
+++ App.js
@@ -2,8 +2,9 @@ import React, { createContext, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import RestaurantList from './src/components/RestaurantList';
-import Details from './src/components/Details';
+import RestaurantListScreen from './src/screens/RestaurantListScreen';
+import Details from './src/screens/DetailsScreen';
+import AddNewRestaurantScreen from './src/screens/AddNewRestaurantScreen';
 
 const Stack = createNativeStackNavigator();
 export const RestaurantContext = createContext();
@@ -14,8 +15,9 @@ export default function App() {
     <NavigationContainer>
       <RestaurantContext.Provider value={{ selectedRestaurant, setSelectedRestaurant }}>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={RestaurantList} />
+          <Stack.Screen name="Home" component={RestaurantListScreen} />
           <Stack.Screen name="Details" component={Details} />
+          <Stack.Screen name="NewRestaurant" component={AddNewRestaurantScreen} />
         </Stack.Navigator>
       </RestaurantContext.Provider>
       
